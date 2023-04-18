b4f04b535cd01d15557e3f019bb6a7b7f0e60774
par1ey <nordsoe2003@gmail.com>
2023-04-18 09:34:19 +0200

    add node_modules and counter

---
+++ functions/questioncounter.js
@@ -0,0 +1,19 @@
+
+class counter{
+    constructor(x, y, z, u, i){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.u = u;
+        this.i = i;
+
+    }
+
+    question(){
+        if(this.x != null){
+            this.y += 1;
+        }
+    return this.y;
+    }
+
+}
